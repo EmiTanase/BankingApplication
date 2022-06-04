2c5ef4968e6b403124d923f1a0ddd76107923809
EmiTanase <105556462+EmiTanase@users.noreply.github.com>
2022-06-04 18:32:31 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Account myAccount = new Account("Customer name", "XX0000");
+        myAccount.showMenu();
+    }
+}
